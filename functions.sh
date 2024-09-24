3feadcfd3799bad6b7dd87fe40c3a5465f55fa95
Qperator <oliha.elias@gmail.com>
2024-09-24 18:52:23 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -26,6 +26,7 @@ generate_simple_client_config_install_command() {
     echo "EOF"
     echo "echo 'config.yml successfully installed at /root/ceremonyclient/node/.config/config.yml'"
     echo -e "\e[0m"
+    echo "💻 You can run your Slave now with clustermaster.bash! 👏"
     echo "#######################👆  END - DONT COPY THIS LINE  👆######################"
     echo "##################################################################################"
 }
